889c133ba889b70f01ec4a02ae65d61f0bfc3c95
rizzkaaa <joremifasollasido@gmail.com>
2025-06-01 22:25:29 +0700

    halaman-daftar-finish-design

--- menu/daftar/index.php
+++ menu/daftar/index.php
@@ -18,17 +18,28 @@
         <input type="radio" name="peran" id="dosen" value="dosen">
         <label for="dosen"><img src="../../assets/image/dosen-btn.png" alt=""></label>
       </div>
-  
-      <div class="wrap-input-data" id="mahasiswa">
-          <input type="text" name="nama_mhs" id="nama_mhs">
-          <input type="text" name="nim" id="nim">
-          <input type="text" name="univ" id="univ">
-          <div class="btn"><img src="../../assets/image/btn-lanjut.png" alt=""></div>
-        </div>
+
+      <!-- <div class="wrap-input-data" id="mahasiswa">
+        <input type="text" name="nama_mhs" id="nama_mhs">
+        <input type="text" name="npm" id="npm">
+        <input type="text" name="univ" id="univ">
+        <div class="btn"><img src="../../assets/image/btn-lanjut.png" alt=""></div>
+      </div> -->
+
+      <div class="wrap-input-data" id="dosen">
+        <input type="text" name="nama_dosen" id="nama_dosen">
+        <input type="text" name="nip" id="nip">
+        <input type="text" name="univ" id="univ">
+        <div class="btn"><img src="../../assets/image/btn-lanjut.png" alt=""></div>
       </div>
 
+      <div class="wrap-input-data" id="pw">
+        <input type="text" name="username">
+        <input type="text" name="password">
+        <a href="#dosen" class="btn"><img src="../../assets/image/btn-simpan.png" alt=""></a>
+      </div>
     </form>
-  </d>
+  </div>
 
   <!--<script>
       const link = document.querySelectorAll(".container a");
