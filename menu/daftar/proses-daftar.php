6e543a83725145c0a1e19c1b653b4805e905865a
rizzkaaa <joremifasollasido@gmail.com>
2025-06-03 09:08:54 +0700

    daftar_db

--- menu/daftar/proses-daftar.php
+++ menu/daftar/proses-daftar.php
@@ -1,18 +1,44 @@
 <?php
-echo "<br>" . $_POST['peran'];
-echo "<br>" . $_POST['nama_mhs'];
-echo "<br>" . $_POST['npm'];
-echo "<br>" . $_POST['univ_mhs'];
-echo "<br>" . $_POST['username'];
-echo "<br>" . $_POST['password'];
 
-// if(!isset($_POST['peran'])){
-//     header("Location: ./?pesan=status-gagal");
-// }
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+include '../../db.php';
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (!isset($_POST['peran'])) {
+        header("Location: ./?pesan=status-gagal");
+    }
 
-// if($_POST['peran'] == 'mahasiswa'){
-//     echo $_POST['peran'];
-// } else{
-//     echo $_POST['peran'];
-// }
+    if ($_POST['peran'] == 'mahasiswa') {
+        $id_mhs = generateID();
+        $id_user = generateID();
+        $nama = mysqli_real_escape_string($connect, $_POST['nama_mhs']);
+        $npm = mysqli_real_escape_string($connect, $_POST['npm']);
+        $univ = mysqli_real_escape_string($connect, $_POST['univ_mhs']);
+        $usn = mysqli_real_escape_string($connect, $_POST['username']);
+        $pw = md5($_POST['password']);
+        $peran = mysqli_real_escape_string($connect, $_POST['peran']);
+
+        $insertUser = mysqli_query($connect, "INSERT INTO users (id_user, username, password, peran) VALUES ('$id_user', '$usn', '$pw', '$peran')");
+        $insertData = mysqli_query($connect, "INSERT INTO mahasiswa (id_mhs, nama_mhs, npm, univ, id_user) VALUES ('$id_mhs',' $nama', '$npm', '$univ', '$id_user')");
+
+
+    } else {
+        $id_dosen = generateID();
+        $id_user = generateID();
+        $nama = mysqli_real_escape_string($connect, $_POST['nama_dosen']);
+        $nip = mysqli_real_escape_string($connect, $_POST['nip']);
+        $univ = mysqli_real_escape_string($connect, $_POST['univ_dosen']);
+        $usn = mysqli_real_escape_string($connect, $_POST['username']);
+        $pw = md5($_POST['password']);
+        $peran = mysqli_real_escape_string($connect, $_POST['peran']);
+
+        $insertUser = mysqli_query($connect, "INSERT INTO users (id_user, username, password, peran) VALUES ('$id_user', '$usn', '$pw', '$peran')");
+        $insertData = mysqli_query($connect, "INSERT INTO dosen (id_dosen, nama_dosen, nip, univ, id_user) VALUES ('$id_dosen',' $nama', '$nip', '$univ', '$id_user')");
+    }
+
+    if ($insertUser && $insertData) {
+        header("Location: ../");
+        exit;
+    }
+}
 ?>
